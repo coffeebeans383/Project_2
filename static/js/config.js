7a18799b9bb8c0811fd539b7344f75e47faf10e5
coffeebeans383 <trots804@gmail.com>
2020-09-22 17:29:09 -0400

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,6 +1,6 @@
 // API key
 const API_KEY = "pk.eyJ1IjoiYjNhbnMiLCJhIjoiY2tla2szMG1zMDVpODJybjYxem93c3JveSJ9.uyCNY3wnBC9Xu3cApmVlmg";
-
+const YOUR_API_KEY = "AIzaSyAZxl9enc6fvinOicCzKke3ZFedIjUT8Qg"
 var api_nash = "7fn2kqmzrpitqvfun958tdrql"
 
 //secret key
